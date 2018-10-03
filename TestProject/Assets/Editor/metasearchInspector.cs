aba69ab2387fefc6bb4269cf0a0fe9a419ea4f05
octopus7 <octopus7net@gmail.com>
2018-10-04 05:01:35 +0900

    refactoring

--- TestProject/Assets/Editor/metasearchInspector.cs
+++ TestProject/Assets/Editor/metasearchInspector.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using UnityEditor;
 using System.Collections.Generic;
@@ -12,91 +12,91 @@ public class metasearchtEditor : Editor
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-        metasearch myTarget = (metasearch)target;
+        metasearch metaInspector = (metasearch)target;
+        if (GUILayout.Button("Find Assets")) metaInspector.log = FindAssets(metaInspector.keyword);
+        if (GUILayout.Button("Scan All assets")) metaInspector.log = ScanAll();
+        if (GUILayout.Button("Find GUID Reverse REF")) metaInspector.log = FindRef(metaInspector.guid);
+        if (GUILayout.Button("Find Orphaned")) metaInspector.log = FindOrphaned();
+    }
 
-        if (GUILayout.Button("FindAssets"))
+    private string FindOrphaned()
+    {
+        string log = "Orphaned";
+
+        HashSet<string> allhash = new HashSet<string>();
+        foreach (var pair in dicRefGuid)
         {
-            string [] guids = AssetDatabase.FindAssets(myTarget.keyword);
-            myTarget.log = "Find : " + myTarget.keyword;
-            foreach (string guid in guids)
+            var hashset = pair.Value;
+            allhash.UnionWith(hashset);
+        }
+        foreach (var pair2 in dicRefGuid)
+        {
+            string guid = pair2.Key;
+            if (pair2.Value.Count == 0 && !allhash.Contains(guid))
             {
                 string path = AssetDatabase.GUIDToAssetPath(guid);
-                myTarget.log += "\n" + guid + " : " + path;
+                if (path.Contains("/Editor/")) continue;
+                if (AssetDatabase.IsValidFolder(path)) continue;
+
+                log += "\n" + guid + " : " + path;
             }
         }
+        return log;
+    }
 
-        if (GUILayout.Button("Scan All assets"))
+    private string ScanAll()
+    {
+        // Retrieves only the assets of the project
+        List<string> paths = new List<string>(AssetDatabase.GetAllAssetPaths());
+        List<string> assetsPaths = paths.Where(item => item.Contains("Assets/")).ToList();
+
+        string log = "";
+        dicRefGuid.Clear();
+
+        int countAssets = 0;
+        int countREFs = 0;
+
+        foreach (string path in assetsPaths)
         {
-            // Retrieves only the assets of the project
-            List<string> paths = new List<string>(AssetDatabase.GetAllAssetPaths());
-            List<string> assetsPaths = paths.Where(item => item.Contains("Assets/")).ToList();
+            countAssets++;
 
-            string log = "";
-            dicRefGuid.Clear();
+            string ownguid = AssetDatabase.AssetPathToGUID(path);
+            if (countAssets < 10) log += "\n" + ownguid + " : " + path;
 
-            int countAssets = 0;
-            int countREFs = 0;
+            dicRefGuid[ownguid] = new HashSet<string>();
+            // Do not recursive search because you need depth.
+            string[] refPaths = AssetDatabase.GetDependencies(path, false);
 
-            foreach (string path in assetsPaths)
+            foreach (string refpath in refPaths)
             {
-                countAssets++;
-
-                string ownguid = AssetDatabase.AssetPathToGUID(path);
-                if(countAssets<10) log += "\n" + ownguid + " : " + path ;                
-
-                dicRefGuid[ownguid] = new HashSet<string>();
-                // Do not recursive search because you need depth.
-                string[] refPaths = AssetDatabase.GetDependencies(path, false); 
-
-                foreach (string refpath in refPaths)
-                {
-                    countREFs++;
-                    if (refpath == path) continue;
-                    string refguid = AssetDatabase.AssetPathToGUID(refpath);
-                    if (countAssets < 10) log += "\n\t - " + refguid + " : " + refpath;
-                    dicRefGuid[ownguid].Add(refguid);
-                }
+                countREFs++;
+                if (refpath == path) continue;
+                string refguid = AssetDatabase.AssetPathToGUID(refpath);
+                if (countAssets < 10) log += "\n\t - " + refguid + " : " + refpath;
+                dicRefGuid[ownguid].Add(refguid);
             }
-
-            myTarget.log = countAssets + " assets " + countREFs + " REFs\n" + log;
         }
 
-        if (GUILayout.Button("Find GUID Reverse REF"))
+        return countAssets + " assets " + countREFs + " REFs\n" + log;
+    }
+
+    private static string FindAssets(string keyword)
+    {
+        string[] guids = AssetDatabase.FindAssets(keyword);
+        string log = "Find : " + keyword;
+        foreach (string guid in guids)
         {
-            myTarget.log = FindRef(myTarget.guid);
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            log += "\n" + guid + " : " + path;
         }
-
-        if (GUILayout.Button("Find Orphaned"))
-        {
-            string log = "Orphaned";
-
-            HashSet<string> allhash = new HashSet<string>();
-            foreach (var pair in dicRefGuid)
-            {
-                var hashset = pair.Value;
-                allhash.UnionWith(hashset);
-            }
-            foreach (var pair2 in dicRefGuid)
-            {
-                string guid = pair2.Key;
-                if ( pair2.Value.Count == 0 && !allhash.Contains(guid))
-                {
-                    string path = AssetDatabase.GUIDToAssetPath(guid);
-                    if (path.Contains("/Editor/")) continue;
-                    if (AssetDatabase.IsValidFolder(path)) continue;
-
-                    log += "\n" + guid + " : " + path;
-                }
-            }
-            myTarget.log = log;
-        }
+        return log;
     }
 
     private string FindRef(string guid, int maxDepth = 3)
     {
         string header = guid;
 
-        string ownlog = "["+ guid + "] " + AssetDatabase.GUIDToAssetPath(guid);
+        string ownlog = "[" + guid + "] " + AssetDatabase.GUIDToAssetPath(guid);
 
         HashSet<string> foundGUID = new HashSet<string>();
         HashSet<string> searchGUID = new HashSet<string>();
@@ -106,7 +106,7 @@ public class metasearchtEditor : Editor
 
         for (int depth = 1; depth <= maxDepth; depth++)
         {
-            ownlog += " \r\ndepth :::: " + depth ;
+            ownlog += " \r\ndepth :::: " + depth;
 
             foreach (var pair in dicRefGuid)
             {
